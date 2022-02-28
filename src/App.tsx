6b78d7b7bb99d8882f9b33ee420799e7e379e046
genraven1 <genraven1@gmail.com>
2022-02-28 09:20:03 -0800

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import AllTalentsViewTable from './components/AllTalentsViewTable';
-import ControlledAccordions from './components/AllTalentsViewAccordian';
 
 function App() {
   const [greeting, setGreeting] = useState(null);
@@ -23,22 +22,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <pre>{greeting}</pre>
+        <AllTalentsViewTable />
       </header>
-      {<ControlledAccordions />}
-      {/* <AllTalentsViewTable /> */}
     </div>
   );
 }
---
+++ src/models/Minion.ts
@@ -0,0 +1,4 @@
+export default interface Minion {
+    name: string,
+    
+}
