78ff3be191c56bf6d823e2953444891fce3c8581
genraven1 <genraven1@gmail.com>
2022-03-25 11:15:04 -0500

    update code

--- src/components/CreateTalent.tsx
+++ src/components/CreateTalent.tsx
@@ -1,12 +1,7 @@
 import { Button, Card, CardActions, CardContent, CardHeader, MenuItem, TextField } from "@mui/material";
 import { ChangeEventHandler, FocusEventHandler, FormEventHandler, useState } from "react";
-import Talent, { DefaultTalent } from "../models/Talent";
+import Talent, { Activation, DefaultTalent, Ranked, Tier } from "../models/Talent";
 import TalentService from "../services/TalentService";
-
-enum Ranked {
-    No = 'No',
-    Yes = 'Yes'
-};
 
 interface Props {
     talent?: Talent | null,
@@ -21,7 +16,7 @@ export default function CreateTalent(props: Props) {
         event.preventDefault();
         setTalent((prev_state) => ({
             ...prev_state,
-            [talent.experience]: talent.tier * 5,
+            [talent.experience]: +talent.tier * 5,
         }));
         console.log(talent);
     }
@@ -46,10 +41,61 @@ export default function CreateTalent(props: Props) {
 
     const handleRankedChange: ChangeEventHandler<HTMLInputElement> = (event) => {
         const { value } = event.target;
+        let temp_ranked: Ranked;
+
+        if (value == Ranked.No) {
+            temp_ranked = Ranked.No;
+        } else if (value == Ranked.Yes) {
+            temp_ranked = Ranked.Yes
+        }
 
         setTalent((prev_state) => ({
             ...prev_state,
-            ranked: value === Ranked.Yes,
+            ranked: temp_ranked,
+        }));
+    }
+
+    const handleActivationChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+        const { value } = event.target;
+        let temp_activation: Activation;
+
+        if (value == Activation.Passive) {
+            temp_activation = Activation.Passive;
+        } else if (value == Activation.ActiveAction) {
+            temp_activation = Activation.ActiveAction
+        } else if (value == Activation.ActiveManeuver) {
+            temp_activation = Activation.ActiveManeuver
+        } else if (value == Activation.ActiveIncidental) {
+            temp_activation = Activation.ActiveIncidental
+        } else if (value == Activation.ActiveIncidentalOutOfTurn) {
+            temp_activation = Activation.ActiveIncidentalOutOfTurn
+        }
+
+        setTalent((prev_state) => ({
+            ...prev_state,
+            activation: temp_activation,
+        }));
+    }
+
+    const handleTierChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+        const { value } = event.target;
+        let temp_tier: Tier;
+
+        if (value == Tier.First) {
+            temp_tier = Tier.First;
+        } else if (value == Tier.Second) {
+            temp_tier = Tier.Second
+        } else if (value == Tier.Third) {
+            temp_tier = Tier.Third
+        } else if (value == Tier.Fourth) {
+            temp_tier = Tier.Fourth
+        } else if (value == Tier.Fifth) {
+            temp_tier = Tier.Fifth
+        }
+
+        setTalent((prev_state) => ({
+            ...prev_state,
+            tier: temp_tier,
         }));
     }
 
@@ -66,6 +112,21 @@ export default function CreateTalent(props: Props) {
                             </MenuItem>
                         ))}
                     </TextField>
+                    <TextField name='activation' label='Activation' value={talent.activation ?? Activation.Passive} onBlur={handleBlur} onChange={handleActivationChange} select >
+                        {Object.entries(Activation).map(([key, value]) => (
+                            <MenuItem key={key} value={value}>
+                                {value}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                    <TextField name='tier' label='Tier' value={talent.tier ?? Tier.First} onBlur={handleBlur} onChange={handleTierChange} select >
+                        {Object.entries(Tier).map(([key, value]) => (
+                            <MenuItem key={key} value={value}>
+                                {value}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                    <TextField name='description' label='Description' value={talent.description ?? ''} onBlur={handleBlur} onChange={handleTextChange} multiline />
                 </CardContent>
                 <CardActions>
                     <Button color='primary' type='submit'>SAVE</Button>
--- src/models/Talent.ts
+++ src/models/Talent.ts
@@ -1,12 +1,33 @@
 export default interface Talent {
     name: string,
-    ranked: boolean,
-    activation: string,
-    tier: number,
+    ranked: Ranked,
+    activation: Activation,
+    tier: Tier,
     description: string,
     id: string,
     experience: number
 }
+
+export enum Ranked {
+    No = 'No',
+    Yes = 'Yes'
+};
+
+export enum Activation {
+    Passive = 'Passive',
+    ActiveAction = 'Active (Action)',
+    ActiveManeuver = 'Active (Maneuver)',
+    ActiveIncidental = 'Active (Incidental)',
+    ActiveIncidentalOutOfTurn = 'Active (Incidental, Out of Turn)'
+};
+
+export enum Tier {
+    First = 'First',
+    Second = 'Second',
+    Third = 'Third',
+    Fourth = 'Fourth',
+    Fifth = 'Fifth'
+};
 
 export interface ActorTalent extends Talent {
     ranks: number
@@ -16,9 +37,9 @@ export class DefaultTalent {
     static create(): Talent {
         return {
             name: '',
-            ranked: false,
-            activation: '',
-            tier: 1,
+            ranked: Ranked.No,
+            activation: Activation.Passive,
+            tier: Tier.First,
             description: '',
             id: '',
             experience: 5
