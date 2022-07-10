49b9a69cc1801a76a69882761e78c118813561af
genraven1 <genraven1@gmail.com>
2022-07-09 21:42:40 -0700

    Initial Equipment Commit

--- src/models/equipment/Equipment.ts
+++ src/models/equipment/Equipment.ts
@@ -1,6 +1,20 @@
+import Skill from "../actor/Skill";
+
 export default interface Equipment {
-    id: string,
-    name: string,
-    description: string,
-    price: string,
+    name: string
+    description: string
+    price: string
+    encumbrance: number
+    rarity: number
+}
+
+export interface Armor extends Equipment {
+    soak: number
+    defense: number
+}
+
+export interface Weapon extends Equipment {
+    damage: number
+    skill: Skill
+    critical: number
 }
