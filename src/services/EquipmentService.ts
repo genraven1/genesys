e069649e1918139d75a9bb33ccf43bc998a4723f
genraven1 <genraven1@gmail.com>
2022-08-24 09:07:55 -0500

    update to normal calls

--- src/services/EquipmentService.ts
+++ src/services/EquipmentService.ts
@@ -2,8 +2,6 @@ import axios from "axios";
 import {Path} from "./Path";
 import {Armor} from "../models/equipment/Equipment";
 
-const fs = require('fs');
-
 export default class EquipmentService {
 
     static async createArmor(name: string): Promise<Armor> {
@@ -11,26 +9,11 @@ export default class EquipmentService {
     }
 
     static async getArmors(): Promise<Armor[]> {
-        let armors = [] as Armor[]
-        let armorsFilenames = fs.readdirSync('mock/armor', {withFileTypes: true})
-            .filter((item: { isDirectory: () => any; }) => !item.isDirectory())
-            .map((item: { name: any; }) => item.name)
-        for (const armorName in armorsFilenames) {
-            armors.push(await this.getArmor(armorName))
-        }
-        return armors
+        return await (await axios.get(Path.Armor)).data;
     }
 
     static async getArmor(name: string): Promise<Armor> {
-        let obj;
-        fs.readFile('mock/armor' + name + '.json', 'utf8', function readFileCallback(err: any, data: string) {
-            if (err) {
-                console.log(err);
-            } else {
-                obj = JSON.parse(data);
-            }
-        })
-        return JSON.stringify(obj) as unknown as Armor
+        return await (await axios.get(Path.Armor + '/' + name)).data;
     }
 
     static async updateArmor(name: string, armor: Armor): Promise<Armor> {
