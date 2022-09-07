bd8ee828585832575c1554aed147dd96f1be08a6
genraven1 <genraven1@gmail.com>
2022-09-07 11:59:14 -0500

    Update SkillsService to new Endpoints

--- src/services/SkillService.ts
+++ src/services/SkillService.ts
@@ -5,11 +5,11 @@ import Skill from '../models/actor/Skill';
 export default class SkillService {
     
     static async createSkill(name: string) {
-        return await axios.post(Path.Skills + '/' + name);
+        return await axios.post(Path.Skills + name);
     }
 
     static async getSkill(name: string): Promise<Skill> {
-        return await (await axios.get(Path.Skills + '/' + name)).data;
+        return await (await axios.get(Path.Skills + name)).data;
     }
 
     static async getSkills(): Promise<Skill[]> {
@@ -17,6 +17,6 @@ export default class SkillService {
     }
 
     static async updateSkill(name: string, skill: Skill): Promise<Skill> {
-        return await axios.put(Path.Skills + '/' + name, skill);
+        return await axios.put(Path.Skills + name, skill);
     }
 }
