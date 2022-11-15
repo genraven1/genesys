25447d42a1fff4a7f60a4c589a389ba023d62df7
genraven1 <genraven1@gmail.com>
2022-11-15 17:48:03 -0600

    GEN-50 Add Proficiency Die Values (#36)
    Add Proficiency Die Values

---
+++ src/models/roll/dice/Proficiency.ts
@@ -0,0 +1,14 @@
+export enum Proficiency {
+    ONE = '',
+    TWO = '[success]',
+    THREE = '[success]',
+    FOUR = '[success] [success]',
+    FIVE = '[success] [success]',
+    SIX = '[advantage]',
+    SEVEN = '[success] [advantage]',
+    EIGHT = '[success] [advantage]',
+    NINE = '[success] [advantage]',
+    TEN = '[advantage] [advantage]',
+    ELEVEN = '[advantage] [advantage]',
+    TWELVE = '[success] [triumph]'
+}
