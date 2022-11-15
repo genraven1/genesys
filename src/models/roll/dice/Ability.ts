3e33efc53a2b70c25b6da9ec0c635c7f19c0c0f0
genraven1 <genraven1@gmail.com>
2022-11-15 17:41:35 -0600

    Add Ability Die Values

---
+++ src/models/roll/dice/Ability.ts
@@ -0,0 +1,10 @@
+export enum Ability {
+    ONE = '',
+    TWO = '[success]',
+    THREE = '[success]',
+    FOUR = '[success] [success]',
+    FIVE = '[advantage]',
+    SIX = '[advantage]',
+    SEVEN = '[success] [advantage]',
+    EIGHT = '[advantage] [advantage]'
+}
