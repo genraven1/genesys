2f8a17554af15c7181bdaa15f28e0bbd2118846f
genraven1 <genraven1@gmail.com>
2022-11-15 17:56:02 -0600

    GEN-53 Add Difficulty Die Values (#38)
    Add Difficulty Die Values

---
+++ src/models/roll/dice/Difficulty.ts
@@ -0,0 +1,10 @@
+export enum Difficulty {
+    ONE = '',
+    TWO = '[failure]',
+    THREE = '[failure] [failure]',
+    FOUR = '[threat]',
+    FIVE = '[threat]',
+    SIX = '[threat]',
+    SEVEN = '[threat] [threat]',
+    EIGHT = '[failure]'
+}
