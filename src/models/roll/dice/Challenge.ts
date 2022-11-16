44ea5efe83926cd6c0eb99da7f6f05b26a3d34b0
genraven1 <genraven1@gmail.com>
2022-11-15 18:08:29 -0600

    Add Challenge Die Values

---
+++ src/models/roll/dice/Challenge.ts
@@ -0,0 +1,14 @@
+export enum Challenge {
+    ONE = '',
+    TWO = '[failure]',
+    THREE = '[failure]',
+    FOUR = '[failure] [failure]',
+    FIVE = '[failure] [failure]',
+    SIX = '[threat]',
+    SEVEN = '[threat]',
+    EIGHT = '[failure] [threat]',
+    NINE = '[failure] [threat]',
+    TEN = '[threat] [threat]',
+    ELEVEN = '[threat] [threat]',
+    TWELVE = '[failure] [despair]'
+}
