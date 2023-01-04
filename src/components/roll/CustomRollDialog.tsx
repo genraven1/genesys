4a37757bbafe4cc4358ffd88c06023eb33627b31
genraven1 <genraven1@gmail.com>
2023-01-04 13:54:42 -0600

    Complete Dialog

--- src/components/roll/CustomRollDialog.tsx
+++ src/components/roll/CustomRollDialog.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import ViewRollTable from "./ViewRollTable";
 import Roll, {DefaultRoll} from "../../models/Roll";
 import {useState} from "react";
-import {Button, Dialog, DialogActions, DialogContent, DialogTitle} from "@mui/material";
+import {Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid} from "@mui/material";
 import GenesysDescriptionTypography from "../common/GenesysDescriptionTypography";
 import RollService from "../../services/RollService";
 
@@ -14,6 +14,9 @@ interface Props {
 export default function CustomRollDialog(props: Props) {
     const {open, onClose} = props
     const [roll, setRoll] = useState<Roll>(DefaultRoll.create)
+    const [rollText, setRollText] = useState('dice')
+    const [resultsText, setResultsText] = useState('results')
+    const [results, setResults] = useState(false)
 
     const onChange = (diceRoll: Roll) => {
         setRoll(diceRoll)
@@ -21,18 +24,27 @@ export default function CustomRollDialog(props: Props) {
 
     const onClick = async () => {
         let results = await RollService.roll(roll)
+        setResults(true)
         console.log(results)
     }
+
+    const viewRoll = <GenesysDescriptionTypography text={rollText}/>
+
+    const viewResults = <GenesysDescriptionTypography text={resultsText}/>
 
     return (
         <Dialog open={open} onClose={onClose}>
             <DialogTitle title={'Assemble Dice Roll'} style={{textAlign:'center'}}/>
             <DialogContent>
                 <ViewRollTable roll={roll} onChange={onChange}/>
-                <GenesysDescriptionTypography text={'dice'}/>
+                <Grid container justifyContent={'center'}>
+                    <Grid item>
+                        {results ? viewResults:viewRoll}
+                    </Grid>
+                </Grid>
             </DialogContent>
             <DialogActions>
-                <Button color='primary' variant='contained' onClick={onClick}>Roll</Button>
+                <Button color='primary' variant='contained' onClick={onClick}>ROLL</Button>
                 <Button color='secondary' variant='contained' onClick={onClose}>CANCEL</Button>
             </DialogActions>
         </Dialog>
