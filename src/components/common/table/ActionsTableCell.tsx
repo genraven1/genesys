01ee90085ecab1abadfec42d4f8c4ebe4a905458
genraven1 <genraven1@gmail.com>
2023-06-03 10:40:02 -0500

    Move Preview to Center

--- src/components/common/table/ActionsTableCell.tsx
+++ src/components/common/table/ActionsTableCell.tsx
@@ -1,5 +1,5 @@
 import PreviewIcon from '@mui/icons-material/Preview'
-import {Box, IconButton, TableCell} from "@mui/material";
+import {IconButton, TableCell} from "@mui/material";
 import {Link, LinkProps} from "react-router-dom";
 import {forwardRef, useMemo} from "react";
 import * as React from "react";
@@ -10,19 +10,17 @@ interface Props {
 }
 
 export default function ActionsTableCell(props: Props): JSX.Element {
-    const {name,path} = props
+    const {name, path} = props
 
     const handleView = useMemo(() => forwardRef<any, Omit<LinkProps, 'to'>>((itemProps, ref): React.ReactElement => (
         <Link to={`${path}${name}/view`} ref={ref} {...itemProps} />
-    )),[path, name])
+    )), [path, name])
 
-    return  (
-        <TableCell>
-            <Box component='span'>
-                <IconButton title='View' size='small' component={handleView} style={{ textAlign: 'center' }}>
-                    <PreviewIcon color='primary' fontSize='small' />
-                </IconButton>
-            </Box>
+    return (
+        <TableCell style={{textAlign: 'center'}}>
+            <IconButton title='View' size='small' component={handleView} style={{textAlign: 'center'}}>
+                <PreviewIcon color='primary' fontSize='small'/>
+            </IconButton>
         </TableCell>
     )
 }
