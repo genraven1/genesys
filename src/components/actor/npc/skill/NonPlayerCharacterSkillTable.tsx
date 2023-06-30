b0af8cff7058aa7aa7ef55f7cac998d0236a7519
Garrett Crowe <untgmc@gmail.com>
2023-06-30 01:21:24 -0500

    Update Minion Skill Card

--- src/components/actor/npc/skill/NonPlayerCharacterSkillTable.tsx
+++ src/components/actor/npc/skill/NonPlayerCharacterSkillTable.tsx
@@ -13,9 +13,47 @@ import Paper from "@mui/material/Paper";
 import TableContainer from "@mui/material/TableContainer";
 import NonPlayerCharacter from "../../../../models/actor/npc/NonPlayerCharacter";
 import NonPlayerCharacterEditSkillDialog from "./NonPlayerCharacterEditSkillDialog";
-import {ActorSkill, getCharacteristicRanks, setSkillName} from "../../../../models/actor/Actor";
+import {ActorSkill, ActorType, getCharacteristicRanks, setSkillName} from "../../../../models/actor/Actor";
 import {renderHeaders} from "../../../common/table/TableRenders";
 import {TypographyCenterTableCell} from "../../../common/table/TypographyTableCell";
+import Minion from "../../../../models/actor/npc/Minion";
+import CheckboxTableCell from "../../../common/table/CheckboxTableCell";
+import Nemesis from "../../../../models/actor/npc/Nemesis";
+import ActorService from "../../../../services/ActorService";
+
+interface GroupSkillRowProps {
+    skill: ActorSkill
+    minion: Minion
+}
+
+function GroupSkillRow(props: GroupSkillRowProps) {
+    const {skill, minion} = props
+
+    const onSettingAddition = async (skill: string) => {
+        const copyMinion = {...minion} as Minion
+        copyMinion.group = copyMinion.group.concat(skill)
+        await ActorService.updateMinion(minion?.name!!, minion)
+    }
+
+    const onSettingRemoval = async (skill: string) => {
+        const copyMinion = {...minion} as Minion
+        copyMinion.group.forEach((name, index) => {
+            if (name === skill) {
+                copyMinion.group.splice(index, 1)
+            }
+        })
+        await ActorService.updateMinion(minion?.name!!, minion)
+    }
+
+    return (
+        <TableRow>
+            <TypographyCenterTableCell value={skill.name}/>
+            <CheckboxTableCell value={minion?.group!!.includes(skill.name)}
+                               onAddition={() => onSettingAddition(skill.name)}
+                               onRemoval={() => onSettingRemoval(skill.name)}/>
+        </TableRow>
+    )
+}
 
 interface RowProps {
     skill: ActorSkill
@@ -53,7 +91,20 @@ interface GroupProps {
 export function SkillTypeGroup(props: GroupProps) {
     const {npc, type} = props
     const [open, setOpen] = useState(false)
-    const headers = ['Name', 'Ranks', 'Dice Pool', 'Edit']
+
+    const renderTableHeaders = (): JSX.Element => {
+        if (npc?.type!! === ActorType.Minion) {
+            return renderHeaders(['Name', 'Group Skill'])
+        }
+        return renderHeaders(['Name', 'Ranks', 'Dice Pool', 'Edit'])
+    }
+
+    const renderSkillRow = (skill: ActorSkill, npc: NonPlayerCharacter): JSX.Element => {
+        if (npc?.type!! === ActorType.Minion) {
+            return <GroupSkillRow skill={skill} minion={npc as Minion}/>
+        }
+        return <SkillRow skill={skill} npc={npc}/>
+    }
 
     return (
         <Fragment>
@@ -66,11 +117,12 @@ export function SkillTypeGroup(props: GroupProps) {
                         <Box sx={{margin: 1}}>
                             <Table>
                                 <TableHead>
-                                    {renderHeaders(headers)}
+                                    {renderTableHeaders()}
                                 </TableHead>
                                 <TableBody>
-                                    {(npc?.skills!! || []).filter((skill) => skill.type === type).map((row: ActorSkill) => (
-                                        <SkillRow key={row.name} skill={row} npc={npc}/>
+                                    {(npc?.skills!! || []).filter((skill) => skill.type === type)
+                                        .map((skill: ActorSkill) => (
+                                        renderSkillRow(skill, npc)
                                     ))}
                                 </TableBody>
                             </Table>
