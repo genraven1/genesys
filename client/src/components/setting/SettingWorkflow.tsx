3a35dffc1e537ffd893ae447914e421a9c49e128
genraven1 <genraven1@gmail.com>
2023-08-14 10:20:33 -0500

    update view all skills format

--- client/src/components/setting/SettingWorkflow.tsx
+++ client/src/components/setting/SettingWorkflow.tsx
@@ -10,11 +10,17 @@ export function useFetchAllSettings(): Setting[] {
     const [settings, setSettings] = useState<Setting[]>([])
     useEffect(() => {
         (async (): Promise<void> => {
-            if (settings.length > 0) {return}
+            if (settings.length > 0) {
+                return
+            }
             try {
                 const settingList = await SettingService.getSettings()
-                if (settingList) {setSettings(settingList)}
-            } catch (err) {console.log(err)}
+                if (settingList) {
+                    setSettings(settingList)
+                }
+            } catch (err) {
+                console.log(err)
+            }
         })()
     }, [settings.length, setSettings])
     return settings
@@ -23,30 +29,53 @@ export function useFetchAllSettings(): Setting[] {
 function useFetchSetting(id: number): Setting {
     const [setting, setSetting] = useState<Setting>()
     useEffect(() => {
-        if(!id) {return}
+        if (!id) {
+            return
+        }
         (async (): Promise<void> => {
             try {
                 const settingData = await SettingService.getSetting(id)
-                if (settingData) {setSetting(settingData)}
-            } catch (err) {console.log(err)}
+                if (settingData) {
+                    setSetting(settingData)
+                }
+            } catch (err) {
+                console.log(err)
+            }
         })()
-    },[id, setSetting])
+    }, [id, setSetting])
+    return setting as Setting
+}
+
+export function useFetchCurrentSetting(): Setting {
+    const [setting, setSetting] = useState<Setting>()
+    useEffect(() => {
+        (async (): Promise<void> => {
+            try {
+                const currentSetting = await SettingService.getCurrentSetting()
+                if (currentSetting) {
+                    setSetting(currentSetting)
+                }
+            } catch (err) {
+                console.log(err)
+            }
+        })()
+    }, [setSetting])
     return setting as Setting
 }
 
 export default function SettingWorkflow(): JSX.Element {
-    const { id } = useParams<{ id?: string }>()
+    const {id} = useParams<{ id?: string }>()
     const setting = useFetchSetting(Number(id!!))
 
     const useWorkflowRender = (): JSX.Element => {
         const pathname = useLocation().pathname
         if (pathname.endsWith('/view')) {
-            return <SettingView  setting={setting}/>
-        }
-        else if (pathname.endsWith('/edit')) {
+            return <SettingView setting={setting}/>
+        } else if (pathname.endsWith('/edit')) {
             return <SettingEdit set={setting}/>
+        } else {
+            return <ViewAllSettings/>
         }
-        else {return <ViewAllSettings/>}
     }
 
     return (
--- client/src/components/skills/ViewAllSkills.tsx
+++ client/src/components/skills/ViewAllSkills.tsx
@@ -12,8 +12,10 @@ import ActionsTableCell from "../common/table/ActionsTableCell";
 import {Path} from "../../services/Path";
 import {renderHeaders} from "../common/table/TableRenders";
 import Setting from "../../models/Setting";
-import SettingService from "../../services/SettingService";
 import {TypographyCenterTableCell} from "../common/table/TypographyTableCell";
+import {Button, Card, CardContent, CardHeader, Divider} from "@mui/material";
+import CreateSkillDialog from "./CreateSkillDialog";
+import {useFetchCurrentSetting} from "../setting/SettingWorkflow";
 
 interface Props {
     skill: Skill
@@ -23,7 +25,7 @@ interface Props {
 function Row(props: Props): JSX.Element {
     const {skill, setting} = props
 
-    const renderSettingTableCell = ():JSX.Element => {
+    const renderSettingTableCell = (): JSX.Element => {
         let value = "No"
         if (skill?.settings!!.includes(setting!!)) {
             value = "Yes"
@@ -44,7 +46,8 @@ function Row(props: Props): JSX.Element {
 
 export default function ViewAllSkills() {
     const [skills, setSkills] = useState<Skill[]>([])
-    const [setting, setSetting] = useState<Setting>()
+    const setting = useFetchCurrentSetting()
+    const [openSkillCreationDialog, setOpenSkillCreationDialog] = useState(false)
     const headers = ['Name', 'Type', 'Linked Characteristic', 'Active', 'View']
 
     useEffect(() => {
@@ -57,28 +60,31 @@ export default function ViewAllSkills() {
         })()
     }, [])
 
-    useEffect(() => {
-        (async (): Promise<void> => {
-            const currentSetting = await SettingService.getCurrentSetting()
-            if (!currentSetting) {
-                return
-            }
-            setSetting(currentSetting)
-        })()
-    }, [setSetting])
-
     return (
-        <TableContainer component={Paper}>
-            <Table>
-                <TableHead>
-                    {renderHeaders(headers)}
-                </TableHead>
-                <TableBody>
-                    {skills.sort((a, b) => a.name.localeCompare(b.name)).map((skill: Skill) => (
-                        <Row key={skill.name} skill={skill} setting={setting!!}/>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <Card>
+            <CardHeader
+                style={{textAlign: 'center'}}
+                title={'View All Skills'}
+                action={<Button color='primary' variant='contained'
+                                onClick={(): void => setOpenSkillCreationDialog(true)}>Create Skill</Button>}>
+            </CardHeader>
+            <Divider/>
+            <CardContent>
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead>
+                            {renderHeaders(headers)}
+                        </TableHead>
+                        <TableBody>
+                            {skills.sort((a, b) => a.name.localeCompare(b.name)).map((skill: Skill) => (
+                                <Row key={skill.name} skill={skill} setting={setting!!}/>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </CardContent>
+            {openSkillCreationDialog && <CreateSkillDialog open={openSkillCreationDialog}
+                                                           onClose={(): void => setOpenSkillCreationDialog(false)}/>}
+        </Card>
     )
 }
