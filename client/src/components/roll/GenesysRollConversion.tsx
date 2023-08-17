b481f7989bc099baa5ffe5219e549df55e8944f4
genraven1 <genraven1@gmail.com>
2023-08-17 14:24:31 -0500

    Update adding to text value

--- client/src/components/roll/GenesysRollConversion.tsx
+++ client/src/components/roll/GenesysRollConversion.tsx
@@ -10,54 +10,56 @@ export function GenesysRollConversion(props: RollProps): JSX.Element {
 
     const generateResultText = ():string => {
         let text = ''
+        console.log(roll)
         while (roll.proficiency > 0) {
-            text.concat('[proficiency]')
+            text = text.concat('[proficiency] ')
             roll.proficiency--
         }
         while (roll.ability > 0) {
-            text.concat('[ability]')
+            text = text.concat('[ability] ')
             roll.ability--
         }
         while (roll.boost > 0) {
-            text.concat('[boost]')
+            text = text.concat('[boost] ')
             roll.boost--
         }
         while (roll.challenge > 0) {
-            text.concat('[challenge]')
+            text = text.concat('[challenge] ')
             roll.challenge--
         }
         while (roll.difficulty > 0) {
-            text.concat('[difficulty]')
+            text = text.concat('[difficulty] ')
             roll.difficulty--
         }
         while (roll.setback > 0) {
-            text.concat('[setback]')
+            text = text.concat('[setback] ')
             roll.setback--
         }
         while (roll.success > 0) {
-            text.concat('[success]')
+            text = text.concat('[success] ')
             roll.success--
         }
         while (roll.failure > 0) {
-            text.concat('[failure]')
+            text = text.concat('[failure] ')
             roll.failure--
         }
         while (roll.advantage > 0) {
-            text.concat('[advantage]')
+            text = text.concat('[advantage] ')
             roll.advantage--
         }
         while (roll.threat > 0) {
-            text.concat('[threat]')
+            text = text.concat('[threat] ')
             roll.threat--
         }
         while (roll.triumph > 0) {
-            text.concat('[triumph]')
+            text = text.concat('[triumph] ')
             roll.triumph--
         }
         while (roll.despair > 0) {
-            text.concat('[despair]')
+            text = text.concat('[despair] ')
             roll.despair--
         }
+        console.log(text)
         return text;
     }
 
@@ -75,30 +77,32 @@ export function GenesysResultsConversion(props: ResultsProps): JSX.Element {
 
     const generateResultText = ():string => {
         let text = ''
+        console.log(results)
         while (results.success > 0) {
-            text.concat('[success]')
+            text = text.concat('[success] ')
             results.success--
         }
         while (results.failure > 0) {
-            text.concat('[failure]')
+            text = text.concat('[failure] ')
             results.failure--
         }
         while (results.advantage > 0) {
-            text.concat('[advantage]')
+            text = text.concat('[advantage] ')
             results.advantage--
         }
         while (results.threat > 0) {
-            text.concat('[threat]')
+            text = text.concat('[threat] ')
             results.threat--
         }
         while (results.triumph > 0) {
-            text.concat('[triumph]')
+            text = text.concat('[triumph] ')
             results.triumph--
         }
         while (results.despair > 0) {
-            text.concat('[despair]')
+            text = text.concat('[despair] ')
             results.despair--
         }
+        console.log(text)
         return text;
     }
 
