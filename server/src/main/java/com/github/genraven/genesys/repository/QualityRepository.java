b54eac2f205c491821f44235d8a4fa55a3c50292
genraven1 <genraven1@gmail.com>
2023-09-08 12:19:07 -0500

    Remove Java Files (#113)

--- server/src/main/java/com/github/genraven/genesys/repository/QualityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.genraven.genesys.repository;
-
-import com.github.genraven.genesys.model.actor.equipment.Quality;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QualityRepository extends JpaRepository<Quality, Long> {
-}
--- server/src/main/java/com/github/genraven/genesys/repository/TalentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.genraven.genesys.repository;
-
-import com.github.genraven.genesys.model.actor.Talent;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TalentRepository extends JpaRepository<Talent, Long> {
-}
