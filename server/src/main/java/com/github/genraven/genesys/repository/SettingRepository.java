aecc86bf948935d10b898ed9679594c7906eac50
genraven1 <genraven1@gmail.com>
2023-09-08 12:18:47 -0500

    Remove Java Files

--- server/src/main/java/com/github/genraven/genesys/repository/SettingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.genraven.genesys.repository;
-
-import com.github.genraven.genesys.model.Setting;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SettingRepository extends JpaRepository<Setting, Long> {
-}
