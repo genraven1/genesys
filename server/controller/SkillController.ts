2fd1820a42e99bb266d9f68a896a978f3a8d41d0
genraven1 <genraven1@outlook.com>
2023-09-14 10:51:52 -0500

    add skill controller

--- server/controller/SkillController.ts
+++ server/controller/SkillController.ts
@@ -1,3 +1,17 @@
 
 
-export const getAllSkills();
+export const getAllSkills = (req, res) => {
+
+}
+
+export const createSkill = (req, res) => {
+
+}
+
+export const getSkill = (req, res) => {
+
+}
+
+export const updateSkill = (res, req) => {
+
+}
