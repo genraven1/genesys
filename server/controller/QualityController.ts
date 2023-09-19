adc12392e8e53fde3393684e2ed643d07eacbb10
genraven1 <genraven1@outlook.com>
2023-09-19 10:48:12 -0500

    add quality routes

--- server/controller/QualityController.ts
+++ server/controller/QualityController.ts
@@ -16,3 +16,29 @@ export const createQuality = (req, res) => {
     let result = await collection.insertOne(newDocument);
     res.send(result).status(204);
 }
+
+export const getQuality = (req, res) => {
+    let collection = await db.collection(QUALITY_COLLECTION);
+    let query = {_id: new ObjectId(req.params.id)};
+    let result = await collection.findOne(query);
+    
+    if (!result) res.sned('Not Found').status(404);
+    else res.send(result).status(200);
+}
+
+export const updateQuality = (req, res) => {
+    const query = {_id: new ObjectId(req.params.id)};
+    const updates = {
+        $set: {
+            name: req.body.name,
+            description: req.body.description,
+            passive: req.body.passive,
+            cost: req.body.cost,
+            armor: req.body.armor,
+            weapon: req.body.weapon
+        }
+    };
+    let collection = await db.collection(QUALITY_COLLECTION);
+    let result = await collection.updateOne(query, updates);
+    res.send(result).status(200);
+}
