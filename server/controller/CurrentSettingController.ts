23dd0e5af42db2ff9ff9fc6693f2e5b5470439a3
genraven1 <genraven1@gmail.com>
2023-10-07 10:28:16 -0500

    set up query and value variables

--- server/controller/CurrentSettingController.ts
+++ server/controller/CurrentSettingController.ts
@@ -1,13 +1,18 @@
 import {pool} from "../config/Database.ts";
 
 export const getCurrentSetting = async (req, res) => {
-    const result = await pool.query("SELECT name FROM current;");
+    const query = "SELECT name FROM current;";
+    const result = await pool.query(query);
     res.send(result.rows[0]);
 };
 
 export const setCurrentSetting = async (req, res) => {
     const { id } = req.params;
-    const results = await pool.query("SELECT name FROM setting WHERE id = $1;", [id]);
-    const result = await pool.query("UPDATE current SET name = $1, id = $2 RETURNING *;", [results.rows[0]['name'], id])
-    res.send(result.rows[0]);
+    const selectQuery = "SELECT name FROM setting WHERE id = $1;";
+    const selectValues = [id];
+    const results = await pool.query(selectQuery, selectValues);
+    const updateQuery = "UPDATE current SET name = $1, id = $2 RETURNING *;";
+    const updateValues = [results.rows[0]['name'], id];
+    const update = await pool.query(updateQuery, updateValues);
+    res.send(update.rows[0]);
 };
--- server/controller/SettingController.ts
+++ server/controller/SettingController.ts
@@ -1,26 +1,34 @@
 import {pool} from "../config/Database.ts";
 
 export const getAllSettings = async (req, res) => {
-    const results = await pool.query("SELECT * FROM setting;");
+    const query = "SELECT * FROM setting;";
+    const results = await pool.query(query);
     res.send(results.rows);
 };
 
 export const getSetting = async (req, res) => {
     const { id } = req.params;
-    const results = await pool.query("SELECT * FROM setting WHERE id = $1;", [id]);
+    const query = "SELECT * FROM setting WHERE id = $1;";
+    const values = [id];
+    const results = await pool.query(query, values);
     res.send(results.rows);
 };
 
 export const createSetting = async (req, res) => {
     const { name } = req.params;
-    const count = await pool.query("SELECT COUNT(*) FROM setting;");
-    const results = await pool.query("INSERT INTO setting (name, id) VALUES ($1, $2) RETURNING *;", [name, Number(count.rows[0]['count']) + 1]);
+    const countQuery = "SELECT COUNT(*) FROM setting;";
+    const count = await pool.query(countQuery);
+    const insertQuery = "INSERT INTO setting (name, id) VALUES ($1, $2) RETURNING *;";
+    const values = [name, Number(count.rows[0]['count']) + 1];
+    const results = await pool.query(insertQuery, values);
     res.send(results.rows[0]);
 };
 
 export const updateSetting = async (req, res) => {
     const { id } = req.params;
     const { name, magic } = req.body;
-    const result = await pool.query("UPDATE setting SET name = $1, magic = $3 WHERE id = $2 RETURNING *;", [name, id, magic]);
+    const query = "UPDATE setting SET name = $1, magic = $3 WHERE id = $2 RETURNING *;";
+    const values = [name, id, magic];
+    const result = await pool.query(query, values);
     res.send(result.rows[0]);
 };
