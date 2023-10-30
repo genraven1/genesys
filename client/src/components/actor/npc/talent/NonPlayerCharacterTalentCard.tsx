4145a9f070a56207777813a3affeb71758d0e21b
Garrett <genraven1@gmail.com>
2023-10-30 18:48:32 -0500

    fix non player character talent card

--- client/src/components/actor/npc/talent/NonPlayerCharacterTalentCard.tsx
+++ client/src/components/actor/npc/talent/NonPlayerCharacterTalentCard.tsx
@@ -1,21 +1,55 @@
-import NonPlayerActor from "../../../../models/actor/npc/NonPlayerActor";
-import {Card, CardContent} from "@mui/material";
+import NonPlayerActor, { SingleNonPlayerCharacter } from "../../../../models/actor/npc/NonPlayerActor";
+import {Button, Card, CardContent} from "@mui/material";
 import NonPlayerCharacterTalentTable from "./NonPlayerCharacterTalentTable";
-import * as React from "react";
 import CenteredCardHeader from "../../../common/card/CenteredCardHeader";
 import GenesysDescriptionTypography from "../../../common/typography/GenesysDescriptionTypography";
+import { Fragment, useState } from "react";
+import { useLocation } from "react-router-dom";
+import Minion from "../../../../models/actor/npc/Minion";
+import { ActorType } from "../../../../models/actor/Actor";
+import MinionTalentTable from "../minion/MinionTalentTable";
+import TalentSelectionDialog from "../../common/talent/TalentSelectionDialog";
 
 interface Props {
     npc: NonPlayerActor
 }
+
 export default function NonPlayerCharacterTalentCard(props: Props): JSX.Element {
     const {npc} = props
+    const [openSelectTalentDialog, setOpenSelectTalentDialog] = useState(false)
+    const pathname = useLocation().pathname
 
     const renderTable = (): JSX.Element => {
-        if (npc?.talents!!.length === 0) {
-            return <GenesysDescriptionTypography text={'None'}/>
+        switch (npc?.type!!) {
+            case ActorType.Minion:
+                let minion = npc as Minion
+                if (minion.talents!!.length === 0){
+                    return <GenesysDescriptionTypography text={'None'}/>
+                }
+                return <MinionTalentTable minion={minion}/>
+            case ActorType.Rival:
+            case ActorType.Nemesis:
+                let single = npc as SingleNonPlayerCharacter
+                if (single?.talents!!.length === 0) {
+                    return <GenesysDescriptionTypography text={'None'}/>
+                }
+                return <NonPlayerCharacterTalentTable npc={single} />
         }
-        return <NonPlayerCharacterTalentTable npc={npc} />
+        return <Fragment/>
+    }
+
+    const renderButton = (): JSX.Element => {
+        if (pathname.endsWith('/edit')) {
+            return (
+                <Fragment>
+                    <Button color='primary' variant='contained' onClick={(): void => setOpenSelectTalentDialog(true)}>Add Talent</Button>
+                    {openSelectTalentDialog && <TalentSelectionDialog actor={npc} open={openSelectTalentDialog}
+                                                                      onClose={(): void => setOpenSelectTalentDialog(false)}/>}
+                </Fragment>
+            )
+        } else {
+            return <Fragment/>
+        }
     }
 
     return (
@@ -23,6 +57,7 @@ export default function NonPlayerCharacterTalentCard(props: Props): JSX.Element 
             <CenteredCardHeader title={'Talents'}/>
             <CardContent>
                 {renderTable()}
+                {renderButton()}
             </CardContent>
         </Card>
     )
