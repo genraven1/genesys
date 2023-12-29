13dcfdbde6b4217db01af36d77540a7199e8f095
genraven1 <genraven1@outlook.com>
2023-12-29 10:12:59 -0600

    Add collections to documents

--- java-server/src/main/java/com/github/genraven/genesys/domain/Activation.java
+++ java-server/src/main/java/com/github/genraven/genesys/domain/Activation.java
@@ -7,13 +7,12 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum Activation {
-    PASSIVE("Passive");
+    PASSIVE("Passive"),
+    ACTIVE_ACTION("Active (Action)"),
+    ACTIVE_MANEUVER("Active (Maneuver)"),
+    ACTIVE_INCIDENTAL("Active (Incidental)"),
+    ACTIVE_INCIDENTAL_OUT_OF_TURN("Active (Incidental, Out of Turn)");
 
     @JsonValue
     private final String label;
 }
-
-//ActiveAction = 'Active (Action)',
-//ActiveManeuver = 'Active (Maneuver)',
-//ActiveIncidental = 'Active (Incidental)',
-//ActiveIncidentalOutOfTurn = 'Active (Incidental, Out of Turn)'
--- java-server/src/main/java/com/github/genraven/genesys/domain/Talent.java
+++ java-server/src/main/java/com/github/genraven/genesys/domain/Talent.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 
 @Data
-@Document
+@Document(collection = "talents")
 public class Talent {
 
     public Talent(final String name) {
@@ -17,6 +17,7 @@ public class Talent {
     @Id
     private Long id;
     private String name;
+    private Activation activation;
     private boolean ranked;
     private String summary;
     private String description;
