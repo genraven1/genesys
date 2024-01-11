2ebda9658c2c36cac67bdd9a72d0757c34e5d0f7
genraven1 <genraven1@outlook.com>
2024-01-11 12:18:01 -0600

    update settings

--- java-server/src/main/java/com/github/genraven/genesys/router/SettingRouter.java
+++ java-server/src/main/java/com/github/genraven/genesys/router/SettingRouter.java
@@ -4,29 +4,41 @@ import com.github.genraven.genesys.constants.SettingConstants;
 import com.github.genraven.genesys.handler.SettingHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static com.github.genraven.genesys.constants.CommonConstants.ID_WITH_BRACKETS;
 import static com.github.genraven.genesys.constants.SettingConstants.CURRENT;
+import static com.github.genraven.genesys.constants.SettingConstants.SETTING_PATH;
 
 @Configuration
 public class SettingRouter {
 
+//    @Bean
+//    RouterFunction<ServerResponse> settingRoutes(final SettingHandler settingHandler) {
+//        return RouterFunctions.route().path(SettingConstants.SETTING_PATH, builder -> builder
+//                .GET(ID_WITH_BRACKETS, settingHandler::getSettingById))
+//                .GET(settingHandler::getAllSettings)
+//                .POST(settingHandler::createSetting)
+//                .build();
+//    }
+//
+//    @Bean
+//    RouterFunction<ServerResponse> currentSettingRoutes(final SettingHandler settingHandler) {
+//        return RouterFunctions.route().path(CURRENT, builder -> builder
+//                .GET(settingHandler::getCurrentSetting))
+//                .build();
+//    }
+
     @Bean
-    RouterFunction<ServerResponse> settingRoutes(final SettingHandler settingHandler) {
-        return RouterFunctions.route().path(SettingConstants.SETTING_PATH, builder -> builder
-                .GET(ID_WITH_BRACKETS, settingHandler::getSettingById))
-                .GET(settingHandler::getAllSettings)
-                .POST(settingHandler::createSetting)
-                .build();
+    RouterFunction<ServerResponse> currentSettingRoute(final SettingHandler settingHandler) {
+        return RouterFunctions.route(RequestPredicates.GET(CURRENT), settingHandler::getCurrentSetting);
     }
 
     @Bean
-    RouterFunction<ServerResponse> currentSettingRoutes(final SettingHandler settingHandler) {
-        return RouterFunctions.route().path(CURRENT, builder -> builder
-                .GET(settingHandler::getCurrentSetting))
-                .build();
+    RouterFunction<ServerResponse> getAllSettings(final SettingHandler settingHandler) {
+        return RouterFunctions.route(RequestPredicates.GET(SETTING_PATH), settingHandler::getAllSettings);
     }
 }
