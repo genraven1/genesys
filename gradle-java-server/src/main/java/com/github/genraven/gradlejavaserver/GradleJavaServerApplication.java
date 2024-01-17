f4ec6ec8e4d25813fd74f53eb49902f3917014f5
genraven1 <genraven1@outlook.com>
2024-01-17 13:52:15 -0600

    add gradle files

---
+++ gradle-java-server/src/main/java/com/github/genraven/gradlejavaserver/GradleJavaServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.genraven.gradlejavaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleJavaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleJavaServerApplication.class, args);
+	}
+
+}
