05ae9269bfe03b0a990b9b9ee7ba7e950a8070d2
genraven1 <genraven1@gmail.com>
2024-03-06 08:40:37 -0600

    Migrate Player Armor Endpoint

--- client/src/services/ActorService.ts
+++ client/src/services/ActorService.ts
@@ -36,10 +36,6 @@ export default class ActorService {
         return await (await axios.put(ActorPath.Player + name, player)).data;
     }
 
-    static async createPlayerArmor(name: string, armor: ActorArmor): Promise<Player> {
-        return await (await axios.post(ActorPath.Player + name + '/armor', armor)).data;
-    }
-
     static async createNemesis(name: string): Promise<Nemesis> {
         return await (await axios.post( ActorPath.Nemesis + name)).data;
     }
