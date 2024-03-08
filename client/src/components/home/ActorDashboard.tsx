c2240196fd0d61b1d8351ddcee5f81ddd617e45a
genraven1 <genraven1@gmail.com>
2024-03-07 23:31:25 -0600

    GEN-258 Move Actors to Actor Tab (#175)
    Move Actors to Actor Tab

---
+++ client/src/components/home/ActorDashboard.tsx
@@ -0,0 +1,56 @@
+import {ActorPath} from "../../services/Path";
+import {Card, CardContent, CardHeader, Grid} from "@mui/material";
+import * as React from "react";
+import {useState} from "react";
+import CreateActorDialog from "../actor/npc/CreateActorDialog";
+import ExpansionList from "../navigation/ExpansionList";
+import CenteredCardHeader from "../common/card/CenteredCardHeader";
+import {ActorType} from "../../models/actor/Actor";
+
+export default function ActorDashboard(): JSX.Element {
+    const [openMinionCreationDialog, setOpenMinionCreationDialog] = useState(false)
+    const [openRivalCreationDialog, setOpenRivalCreationDialog] = useState(false)
+    const [openNemesisCreationDialog, setOpenNemesisCreationDialog] = useState(false)
+    const [openPlayerCreationDialog, setOpenPlayerCreationDialog] = useState(false)
+
+    return (
+        <Card>
+            <CenteredCardHeader title={'Actor Dashboard'}/>
+            <CardContent>
+                <Grid container justifyContent={'center'}>
+                    <Card>
+                        <CardHeader style={{textAlign: 'center'}} title={'Actors'}/>
+                        <CardContent>
+                            <Grid container justifyContent={'center'}>
+                                <ExpansionList header={'View Minions'} viewTitle={'View Minions'} to={ActorPath.Minion}
+                                               dialogTitle={'Create Minions'}
+                                               onClick={(): void => setOpenMinionCreationDialog(true)}/>
+                                <ExpansionList header={'View Rivals'} viewTitle={'View Rivals'} to={ActorPath.Rival}
+                                               dialogTitle={'Create Rival'}
+                                               onClick={(): void => setOpenRivalCreationDialog(true)}/>
+                                <ExpansionList header={'View Nemeses'} viewTitle={'View Nemeses'} to={ActorPath.Nemesis}
+                                               dialogTitle={'Create Nemesis'}
+                                               onClick={(): void => setOpenNemesisCreationDialog(true)}/>
+                                <ExpansionList header={'View Players'} viewTitle={'View Players'} to={ActorPath.Player}
+                                               dialogTitle={'Create Player'}
+                                               onClick={(): void => setOpenPlayerCreationDialog(true)}/>
+                            </Grid>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </CardContent>
+            {openMinionCreationDialog && <CreateActorDialog open={openMinionCreationDialog}
+                                                            onClose={(): void => setOpenMinionCreationDialog(false)}
+                                                            actorType={ActorType.Minion}/>}
+            {openRivalCreationDialog && <CreateActorDialog open={openRivalCreationDialog}
+                                                           onClose={(): void => setOpenRivalCreationDialog(false)}
+                                                           actorType={ActorType.Rival}/>}
+            {openNemesisCreationDialog && <CreateActorDialog open={openNemesisCreationDialog}
+                                                             onClose={(): void => setOpenNemesisCreationDialog(false)}
+                                                             actorType={ActorType.Nemesis}/>}
+            {openPlayerCreationDialog && <CreateActorDialog open={openPlayerCreationDialog}
+                                                            onClose={(): void => setOpenPlayerCreationDialog(false)}
+                                                            actorType={ActorType.Player}/>}
+        </Card>
+    )
+}
