e274dfd0fca61669ea3708262789acc9db31a411
genraven1 <genraven1@gmail.com>
2024-03-10 23:15:10 -0500

    GEN-182 View Critical Injury (#179)
    View Critical Injury

---
+++ client/src/components/injuries/InjuryView.tsx
@@ -0,0 +1,45 @@
+import Injury from "../../models/Injury";
+import {Card, CardContent, CardHeader, Divider, Grid, IconButton} from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import {ViewFieldCard} from "../common/ViewFieldCard";
+import * as React from "react";
+import {useNavigate} from "react-router-dom";
+import {Path} from "../../services/Path";
+
+interface Props {
+    injury: Injury
+}
+
+export default function InjuryView(props: Props):JSX.Element {
+    const {injury} = props
+    let navigate = useNavigate()
+
+    const onEdit = () => {
+        navigate(Path.Injury + injury.name + '/edit')
+    }
+
+    return (
+        <Card>
+            <CardHeader
+                style={{textAlign: 'center'}}
+                title={injury.name}
+                action={<IconButton title='Edit' size='small' onClick={(): void => onEdit()}>
+                    <EditIcon color='primary' fontSize='small'/>
+                </IconButton>}>
+            </CardHeader>
+            <CardContent>
+                <Grid container justifyContent={'center'}>
+                    <Grid container spacing={2}>
+                        <ViewFieldCard name={'Description'} value={injury.description}/>
+                    </Grid>
+                    <Divider/>
+                    <Grid container spacing={2}>
+                        <ViewFieldCard name={'Difficulty'} value={injury.difficulty}/>
+                        <ViewFieldCard name={'Min'} value={String(injury.min)}/>
+                        <ViewFieldCard name={'Max'} value={String(injury.max)}/>
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    )
+}
