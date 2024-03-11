cbd53e2db4f5200bcb3854fd67de7fe9c235e1bd
genraven1 <genraven1@outlook.com>
2024-03-10 21:52:25 -0500

    Finish Critical Injury Dialog

---
+++ client/src/components/injuries/CreateInjuryDialog.tsx
@@ -0,0 +1,40 @@
+import {Dialog, DialogContentText, DialogTitle, TextField} from "@mui/material"
+import {GenesysDialogActions} from "../common/dialog/GenesysDialogActions";
+import {ChangeEvent, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {Path} from "../../services/Path";
+import InjuryService from "../../services/InjuryService";
+
+
+interface Props {
+    open: boolean
+    onClose: () => void
+}
+
+export default function CreateInjuryDialog(props: Props) {
+    const {open, onClose} = props
+
+    const [name,setName] = useState('')
+    let navigate = useNavigate()
+
+    const handleCreate = async (): Promise<void> => {
+        let injury = await InjuryService.createInjury(name)
+        navigate(Path.Injury + injury.name + '/edit')
+        onClose()
+    }
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        const { value } = event.target
+        setName(value)
+    }
+
+    return (
+        <Dialog open={open} onClose={onClose}>
+            <DialogTitle>Create Critical Injury</DialogTitle>
+            <DialogContentText>
+                <TextField onChange={onChange} value={name} required/>
+            </DialogContentText>
+            <GenesysDialogActions handleCreate={handleCreate} onClose={onClose}/>
+        </Dialog>
+    )
+}
