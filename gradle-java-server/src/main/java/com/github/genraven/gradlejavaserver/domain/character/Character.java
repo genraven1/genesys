cf47ca76de27dd4d13306bf7dbcdfbd47bac9eac
genraven1 <genraven1@gmail.com>
2024-03-12 11:41:15 -0500

    GEN-281 Add Wounds (#186)
    Add Wounds

--- gradle-java-server/src/main/java/com/github/genraven/gradlejavaserver/domain/character/Character.java
+++ gradle-java-server/src/main/java/com/github/genraven/gradlejavaserver/domain/character/Character.java
@@ -9,9 +9,10 @@ public class Character {
     protected Character() {}
 
     public Character(final Actor actor) {
-
+        this.setWounds(new Wounds(0, actor.getWounds()));
     }
 
+    private Wounds wounds;
     private StatusEffect disoriented = new StatusEffect(StatusEffect.Type.DISORIENTED);
     private StatusEffect immobilized = new StatusEffect(StatusEffect.Type.IMMOBILIZED);
     private StatusEffect staggered = new StatusEffect(StatusEffect.Type.STAGGERED);
