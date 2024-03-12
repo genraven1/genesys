5896637707f994fd649ef5d92e66a9d8ee902e1c
genraven1 <genraven1@outlook.com>
2024-03-12 11:33:12 -0500

    Add Wounds

--- client/src/models/character/Character.ts
+++ client/src/models/character/Character.ts
@@ -1,7 +1,8 @@
 import StatusEffect from "./StatusEffect";
+import Wounds from "./Wounds";
 
 export default interface Character {
-
+    wounds: Wounds
     disoriented: StatusEffect
     immobilized: StatusEffect
     staggered: StatusEffect
---
+++ client/src/models/character/Wounds.ts
@@ -0,0 +1,4 @@
+export default interface Wounds {
+    wounds: number
+    threshold: number
+}
