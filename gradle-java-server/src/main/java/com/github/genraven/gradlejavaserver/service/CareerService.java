c9c5171e58969667ec34f6a69da12b07e1e92f7e
genraven1 <genraven1@outlook.com>
2024-03-15 11:25:38 -0500

    Add Career Backend

---
+++ gradle-java-server/src/main/java/com/github/genraven/gradlejavaserver/service/CareerService.java
@@ -0,0 +1,37 @@
+package com.github.genraven.gradlejavaserver.service;
+
+import com.github.genraven.gradlejavaserver.domain.actor.player.Career;
+import com.github.genraven.gradlejavaserver.repository.CareerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class CareerService {
+    private final CareerRepository careerRepository;
+
+    @Autowired
+    public CareerService(final CareerRepository careerRepository) {
+        this.careerRepository = careerRepository;
+    }
+
+    public Flux<Career> getAllCareers() {
+        return careerRepository.findAll();
+    }
+
+    public Mono<Career> getCareer(final String name) {
+        return careerRepository.findById(name);
+    }
+
+    public Mono<Career> createCareer(final String name) {
+        return careerRepository.save(new Career(name));
+    }
+
+    public Mono<Career> updateCareer(final String name, final Career career) {
+        return getCareer(name).map(car -> {
+            car.setSkills(career.getSkills());
+            return car;
+        }).flatMap(careerRepository::save);
+    }
+}
