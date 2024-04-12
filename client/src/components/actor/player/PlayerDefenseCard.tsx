9f923b644009f6ed7462e1c5ad68e89b926f17af
genraven1 <genraven1@outlook.com>
2024-04-12 10:12:32 -0500

    Remove old Edit and View Defense Cards

--- client/src/components/actor/player/PlayerDefenseCard.tsx
+++ client/src/components/actor/player/PlayerDefenseCard.tsx
@@ -1,5 +1,4 @@
-import {Card, CardActions, CardContent, Grid} from "@mui/material";
-import InputNumberRangeSelectField from "../../common/InputNumberRangeSelect";
+import {Card, CardContent, Grid} from "@mui/material";
 import {DefenseType} from "../../../models/actor/Defense";
 import CenteredCardHeader from "../../common/card/CenteredCardHeader";
 import GenesysDescriptionTypography from "../../common/typography/GenesysDescriptionTypography";
@@ -40,49 +39,5 @@ export default function PlayerDefenseCard(props: Props) {
                 </Card>
             </Grid>
         </Fragment>
-    )
-}
-
-interface EditProps {
-    defense: number
-    type: DefenseType
-    onChange: (value: number) => void
-}
-
-export function EditDefenseCard(props: EditProps) {
-    const { defense, type, onChange } = props;
-
-    return (
-        <Grid item xs>
-            <Card>
-                <CenteredCardHeader title={type}/>
-                <CardContent>
-                    <GenesysDescriptionTypography text={String(defense)}/>
-                </CardContent>
-                <CardActions>
-                    <InputNumberRangeSelectField defaultValue={defense} min={0} max={5} onCommit={onChange} />
-                </CardActions>
-            </Card>
-        </Grid>
-    )
-}
-
-interface ViewProps {
-    defense: number
-    type: DefenseType
-}
-
-export function ViewDefenseCard(props: ViewProps) {
-    const { defense, type } = props;
-
-    return (
-        <Grid item xs>
-            <Card>
-                <CenteredCardHeader title={type}/>
-                <CardContent>
-                    <GenesysDescriptionTypography text={String(defense)}/>
-                </CardContent>
-            </Card>
-        </Grid>
     )
 }
