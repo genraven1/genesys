54b1a69820e4f33a0760d3da3cfd60a03141d38c
genraven1 <genraven1@outlook.com>
2024-05-23 15:56:13 -0500

    Add Default Party

--- gradle-java-server/src/main/java/com/github/genraven/gradlejavaserver/domain/campaign/Party.java
+++ gradle-java-server/src/main/java/com/github/genraven/gradlejavaserver/domain/campaign/Party.java
@@ -1,4 +1,14 @@
 package com.github.genraven.gradlejavaserver.domain.campaign;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
 public class Party {
+
+    private List<Character> characters = new ArrayList<>();
 }
