5867715d71989e85e8c81613cd7a26eaba23f637
genraven1 <genraven1@outlook.com>
2024-05-31 08:17:16 -0500

    Use Encrypted Values

--- client/src/services/BaseService.ts
+++ client/src/services/BaseService.ts
@@ -1,3 +1,3 @@
 import {createClient} from "@supabase/supabase-js";
 
-export const supabase = createClient("REACT_APP_SUPABASE_URL", "REACT_APP_SUPABASE_KEY");
+export const supabase = createClient("SUPABASE_URL", "SUPABASE_KEY");
