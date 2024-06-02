71202e836b0655877e920f18e349067b00093310
genraven1 <genraven1@gmail.com>
2024-06-02 12:05:20 -0500

    GEN-319 Remove Node files (#223)
    remove node files

--- node-server/routes/NonPlayerActorRoutes.ts
+++
@@ -1,8 +0,0 @@
-import Router from "express-promise-router";
-import {NPC_PATH} from "../utils/Path.ts";
-import {getAllNonPlayerActors} from "../controller/NonPlayerActorController.ts";
-
-export const npaRouter = Router();
-
-npaRouter.route(NPC_PATH)
-    .get(getAllNonPlayerActors)
