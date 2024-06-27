c37afd6bf292d23e780aef4c0236169ce900d099
genraven1 <genraven1@outlook.com>
2024-06-27 10:42:44 -0500

    Add Party Creation to Campaign Creation

--- functions/campaigns/index.ts
+++ functions/campaigns/index.ts
@@ -1,4 +1,5 @@
 import Campaign from "../../src/models/campaign/Campaign";
+import Party from "../../src/models/campaign/Party";
 
 interface Env {
     GENESYS: D1Database;
@@ -12,5 +13,6 @@ export const onRequestGet: PagesFunction<Env> = async (context) => {
 export const onRequestPost: PagesFunction<Env> = async (context) => {
     const createResult = await context.request.json() as Campaign
     const result = await context.env.GENESYS.prepare('INSERT INTO Campaign (campaign_id, name) VALUES (null, ?) RETURNING *').bind(createResult.name).first<Campaign>();
+    result.party = await context.env.GENESYS.prepare('INSERT INTO Party (party_id) VALUES (null) RETURNING *').first<Party>()
     return Response.json(result)
 }
