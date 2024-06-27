392632f296dfbd6717de49632d117add76806cee
genraven1 <genraven1@outlook.com>
2024-06-27 13:19:51 -0500

    Switch Endpoints

--- functions/talents/index.ts
+++ functions/talents/index.ts
@@ -1,16 +1,16 @@
-import Injury from "../../src/models/Injury";
+import Talent from "../../src/models/Talent";
 
 interface Env {
     GENESYS: D1Database;
 }
 
 export const onRequestGet: PagesFunction<Env> = async (context) => {
-    const {results} = await context.env.GENESYS.prepare('SELECT * FROM Injury').all<Injury>();
+    const {results} = await context.env.GENESYS.prepare('SELECT * FROM Talent').all<Talent>();
     return Response.json(results);
 }
 
 export const onRequestPost: PagesFunction<Env> = async (context) => {
-    const createResult = await context.request.json() as Injury
-    const result = await context.env.GENESYS.prepare('INSERT INTO Injury (injury_id, name) VALUES (null, ?) RETURNING *').bind(createResult.name).first<Injury>();
-    return Response.json(result)
+    const createResult = await context.request.json() as Talent;
+    const result = await context.env.GENESYS.prepare('INSERT INTO Talent (talent_id, name) VALUES (null, ?) RETURNING *').bind(createResult.name).first<Talent>();
+    return Response.json(result);
 }
