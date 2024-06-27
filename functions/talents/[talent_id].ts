847f52b2561d7fef213faece572fc2592a477ea6
genraven1 <genraven1@outlook.com>
2024-06-27 13:25:41 -0500

    Add commas to put sql call

--- functions/talents/[talent_id].ts
+++ functions/talents/[talent_id].ts
@@ -14,6 +14,6 @@ export const onRequestGet: PagesFunction<Env> = async (context) => {
 
 export const onRequestPut: PagesFunction<Env> = async (context) => {
     const updatedResult = await context.request.json() as Talent
-    const result = await context.env.GENESYS.prepare('UPDATE Talent SET description = ?2 summary = ?3 activation = ?4 tier = ?5 ranked = ?6 WHERE talent_id = ?1 RETURNING *').bind(context.params.talent_id, updatedResult.description, updatedResult.summary, updatedResult.activation, updatedResult.tier, updatedResult.ranked).first<Talent>();
+    const result = await context.env.GENESYS.prepare('UPDATE Talent SET description = ?2, summary = ?3, activation = ?4, tier = ?5, ranked = ?6 WHERE talent_id = ?1 RETURNING *').bind(context.params.talent_id, updatedResult.description, updatedResult.summary, updatedResult.activation, updatedResult.tier, updatedResult.ranked).first<Talent>();
     return Response.json(result)
 }
