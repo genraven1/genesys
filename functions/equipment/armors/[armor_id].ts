bfb4c8588e8b7d04371dc0671c3b499c97eed936
genraven1 <genraven1@outlook.com>
2024-07-02 09:46:44 -0500

    Update GET Armor Endpoints

--- functions/equipment/armors/[armor_id].ts
+++ functions/equipment/armors/[armor_id].ts
@@ -7,30 +7,32 @@ interface Env {
 }
 
 export const onRequestGet: PagesFunction<Env> = async (context) => {
-    const armor = await context.env.GENESYS.prepare('SELECT\n' +
-        '    aq.armor_id AS "armor_id",\n' +
-        '    a.*,\n' +
-        '    JSON_ARRAY(\n' +
-        '        JSON_OBJECT(\'type\', \'\', \'ranks\', NULL)\n' +
-        '    ) AS "modifiers",\n' +
-        '    JSON_ARRAY(\n' +
-        '        JSON_OBJECT(\n' +
-        '            \'quality_id\', \'\',\n' +
-        '            \'description\', \'\',\n' +
-        '            \'passive\', NULL,\n' +
-        '            \'cost\', NULL,\n' +
-        '            \'armor\', NULL,\n' +
-        '            \'weapon\', NULL,\n' +
-        '            \'modifiers\', JSON_ARRAY(\n' +
-        '                JSON_OBJECT(\'type\', \'\', \'ranks\', NULL)\n' +
-        '            )\n' +
-        '        )\n' +
-        '    ) AS "qualities"\n' +
-        'FROM ArmorQuality AS aq\n' +
-        'JOIN Quality AS q ON aq.quality_id = q.quality_id\n' +
-        'JOIN Armor AS a ON aq.armor_id = a.armor_id\n' +
-        'WHERE aq.armor_id = ?;').bind(context.params.armor_id).first<Armor>();
-    console.log(armor)
+    const query = `SELECT a.*,
+                          JSON_ARRAY(
+                                  JSON_OBJECT('type', am.type, 'ranks', am.ranks)
+                          ) AS "modifiers",
+                          JSON_ARRAY(
+                                  JSON_OBJECT(
+                                          'quality_id', q.quality_id,
+                                          'name', q.name,
+                                          'description', q.description,
+                                          'passive', q.passive,
+                                          'cost', q.cost,
+                                          'armor', q.armor,
+                                          'weapon', q.weapon,
+                                          'ranks', aq.ranks,
+                                          'modifiers', JSON_ARRAY(
+                                                  JSON_OBJECT('type', qm.type, 'ranks', qm.ranks)
+                                                       )
+                                  )
+                          ) AS "qualities"
+                   FROM ArmorQuality AS aq
+                            JOIN ArmorModification am ON aq.armor_id = am.armor_id
+                            JOIN QualityModification qm ON aq.quality_id = qm.quality_id
+                            JOIN Quality AS q ON aq.quality_id = q.quality_id
+                            JOIN Armor AS a ON aq.armor_id = a.armor_id
+                   WHERE aq.armor_id = 1;`
+    const armor = await context.env.GENESYS.prepare(query).bind(context.params.armor_id).first<Armor>();
     return Response.json(armor);
 }
 
