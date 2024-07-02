f3f1e437b1fa5f60e12ccd57e33f66acccbd3aa8
genraven1 <genraven1@outlook.com>
2024-07-02 08:46:34 -0500

    Update GET Armor Endpoints

--- functions/equipment/armors/[armor_id].ts
+++ functions/equipment/armors/[armor_id].ts
@@ -7,16 +7,34 @@ interface Env {
 }
 
 export const onRequestGet: PagesFunction<Env> = async (context) => {
-    const armor = await context.env.GENESYS.prepare('SELECT * FROM Armor WHERE armor_id = ?').bind(context.params.armor_id).first<Armor>();
-    // const {results} = await context.env.GENESYS.prepare(`SELECT * FROM ArmorModification WHERE armor_id = ?`).bind(context.params.armor_id).all<Modifier>();
-    // armor.modifiers = results
-    const {results} = await context.env.GENESYS.prepare(`SELECT * FROM ArmorQuality WHERE armor_id = ?`).bind(context.params.armor_id).all<EquipmentQuality>();
-    armor.qualities = results
+    const armor = await context.env.GENESYS.prepare('SELECT\n' +
+        '    aq.armor_id AS "armor_id",\n' +
+        '    a.*,\n' +
+        '    JSON_ARRAY(\n' +
+        '        JSON_OBJECT(\'type\', \'\', \'ranks\', NULL)\n' +
+        '    ) AS "modifiers",\n' +
+        '    JSON_ARRAY(\n' +
+        '        JSON_OBJECT(\n' +
+        '            \'quality_id\', \'\',\n' +
+        '            \'description\', \'\',\n' +
+        '            \'passive\', NULL,\n' +
+        '            \'cost\', NULL,\n' +
+        '            \'armor\', NULL,\n' +
+        '            \'weapon\', NULL,\n' +
+        '            \'modifiers\', JSON_ARRAY(\n' +
+        '                JSON_OBJECT(\'type\', \'\', \'ranks\', NULL)\n' +
+        '            )\n' +
+        '        )\n' +
+        '    ) AS "qualities"\n' +
+        'FROM ArmorQuality AS aq\n' +
+        'JOIN Quality AS q ON aq.quality_id = q.quality_id\n' +
+        'JOIN Armor AS a ON aq.armor_id = a.armor_id\n' +
+        'WHERE aq.armor_id = ?;').bind(context.params.armor_id).first<Armor>();
     return Response.json(armor);
 }
 
 export const onRequestPut: PagesFunction<Env> = async (context) => {
     const updatedResult = await context.request.json() as Armor;
-    const result = await context.env.GENESYS.prepare('UPDATE Armor SET description = ?2 WHERE injury_id = ?1 RETURNING *').bind(context.params.injury_id, updatedResult.description).first<Armor>();
+    const result = await context.env.GENESYS.prepare('UPDATE Armor SET description = ?2 WHERE armor_id = ?1 RETURNING *').bind(context.params.armor_id, updatedResult.description).first<Armor>();
     return Response.json(result);
 }
