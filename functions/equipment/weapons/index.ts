5ebff2393663a55df222a28dc29515c22e9e0aa4
genraven1 <genraven1@outlook.com>
2024-07-06 10:23:28 -0500

    Update Weapon Endpoint

--- functions/equipment/weapons/index.ts
+++ functions/equipment/weapons/index.ts
@@ -6,14 +6,14 @@ interface Env {
 }
 
 export const onRequestGet: PagesFunction<Env> = async (context) => {
-    const {results} = await context.env.GENESYS.prepare('SELECT * FROM Armor')
+    const {results} = await context.env.GENESYS.prepare('SELECT * FROM Weapon')
         .all<Weapon>();
     return Response.json(results);
 }
 
 export const onRequestPost: PagesFunction<Env> = async (context) => {
     const createResult = await context.request.json() as Weapon
-    const result = await context.env.GENESYS.prepare('INSERT INTO Armor (weapon_id, name) VALUES (null, ?) RETURNING *')
+    const result = await context.env.GENESYS.prepare('INSERT INTO Weapon (weapon_id, name) VALUES (null, ?) RETURNING *')
         .bind(createResult.name)
         .first<Weapon>();
     return Response.json(result)
