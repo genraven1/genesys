bb2d1f2896704ea7ea10279643862e1d72742480
genraven1 <genraven1@outlook.com>
2024-08-21 17:00:44 -0500

    Remove FetchAllSetting Endpoint

--- src/components/setting/SettingWorkflow.tsx
+++ src/components/setting/SettingWorkflow.tsx
@@ -6,26 +6,6 @@ import SettingEdit from "./SettingEdit";
 import SettingView from "./SettingView";
 import ViewAllSettings from "./ViewAllSettings";
 
-export function useFetchAllSettings(): Setting[] {
-    const [settings, setSettings] = useState<Setting[]>([])
-    useEffect(() => {
-        (async (): Promise<void> => {
-            if (settings.length > 0) {
-                return
-            }
-            try {
-                const settingList = await SettingService.getSettings()
-                if (settingList) {
-                    setSettings(settingList)
-                }
-            } catch (err) {
-                console.log(err)
-            }
-        })()
-    }, [settings.length, setSettings])
-    return settings
-}
-
 function useFetchSetting(name: string): Setting {
     const [setting, setSetting] = useState<Setting>()
     useEffect(() => {
