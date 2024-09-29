60b43dcbcf7f52d0c50f38a44b29f16abe50b6b8
genraven1 <genraven1@outlook.com>
2024-09-29 09:12:30 -0500

    Handle Talent Modifiers

--- client/src/components/talents/modifier/TalentModifierCard.tsx
+++ client/src/components/talents/modifier/TalentModifierCard.tsx
@@ -1,4 +1,4 @@
-import {Button, Card, CardContent, TableFooter} from "@mui/material";
+import {Button, Card, CardContent, TableFooter, TextField} from "@mui/material";
 import CenteredCardHeader from "../../common/card/CenteredCardHeader";
 import TableContainer from "@mui/material/TableContainer";
 import Paper from "@mui/material/Paper";
@@ -7,12 +7,14 @@ import {renderSingleRowTableHeader} from "../../common/table/TableRenders";
 import {useLocation} from "react-router-dom";
 import TableBody from "@mui/material/TableBody";
 import TableRow from "@mui/material/TableRow";
-import {TypographyCenterTableCell} from "../../common/table/TypographyTableCell";
 import AddIcon from '@mui/icons-material/Add';
-import {Fragment, useState} from "react";
+import {Fragment, useEffect, useState} from "react";
 import * as React from "react";
-import AddTalentModifierDialog from "./AddTalentModifierDialog";
 import Talent from "../../../models/Talent";
+import Modifier from "../../../models/common/Modifier";
+import ModifierService from "../../../services/ModifierService";
+import TableCell from "@mui/material/TableCell";
+import {Autocomplete} from "@mui/lab";
 
 interface Props {
     talent: Talent
@@ -20,20 +22,24 @@ interface Props {
 
 export default function TalentModifierCard(props: Props) {
     const {talent} = props
-    const [openDialog, setOpenDialog] = useState(false)
     const pathname = useLocation().pathname
     const headers = ['Type', 'Ranks']
+    const [rows, setRows] = useState<Modifier[]>(talent.modifiers)
+    const [typeOptions, setTypeOptions] = useState<string[]>([]);
 
-    const renderTableFooter = (): JSX.Element => {
+    useEffect(() => {
+        (async () => {
+            setTypeOptions(await ModifierService.getModifiers())
+        })()
+    }, [])
+
+    const renderTableFooter = () => {
         if (pathname.endsWith('/edit')) {
             return (
                 <TableFooter>
                     <TableRow key={'Footer'}>
                         <Button variant='contained' color='primary' onClick={addRow} startIcon={<AddIcon/>}>Add
                             Modifier</Button>
-                        {openDialog && <AddTalentModifierDialog open={openDialog}
-                                                                onClose={(): void => setOpenDialog(false)}
-                                                                talent={talent}/>}
                     </TableRow>
                 </TableFooter>
             )
@@ -42,9 +48,24 @@ export default function TalentModifierCard(props: Props) {
         }
     }
 
+    const handleTypeChange = (index: number, value: string) => {
+        const updatedRows = rows.map((row, i) =>
+            i === index ? {...row, type: value} : row
+        );
+        setRows(updatedRows);
+    };
+
+    const handleRanksChange = (index: number, value: string) => {
+        const updatedRows = rows.map((row, i) =>
+            i === index ? {...row, ranks: Number(value)} : row
+        );
+        setRows(updatedRows);
+    };
+
     const addRow = () => {
-        setOpenDialog(true)
-    }
+        const newRow: Modifier = {type: '', ranks: 1};
+        setRows([...rows, newRow]);
+    };
 
     return (
         <Card sx={{"width": 1}}>
@@ -54,10 +75,28 @@ export default function TalentModifierCard(props: Props) {
                     <Table>
                         {renderSingleRowTableHeader(headers)}
                         <TableBody>
-                            {(talent.modifiers).map((modifier) => (
-                                <TableRow key={modifier.type}>
-                                    <TypographyCenterTableCell value={modifier.type}/>
-                                    <TypographyCenterTableCell value={String(modifier.ranks)}/>
+                            {rows.map((row, index) => (
+                                <TableRow key={index}>
+                                    <TableCell>
+                                        <Autocomplete
+                                            options={typeOptions}
+                                            getOptionLabel={(option) => option}
+                                            value={row.type}
+                                            onChange={(e, newValue) => handleTypeChange(index, newValue as string)}
+                                            renderInput={(params) => <TextField {...params} label="Type"
+                                                                                variant="outlined"/>}
+                                            disabled={pathname.endsWith('/view')}
+                                        />
+                                    </TableCell>
+                                    <TableCell style={{textAlign: "center"}}>
+                                        <TextField
+                                            type="number"
+                                            value={row.ranks}
+                                            onChange={(e) => handleRanksChange(index, e.target.value)}
+                                            inputProps={{min: 1, max: 10}}
+                                            disabled={pathname.endsWith('/view')}
+                                        />
+                                    </TableCell>
                                 </TableRow>
                             ))}
                         </TableBody>
@@ -66,5 +105,5 @@ export default function TalentModifierCard(props: Props) {
                 </TableContainer>
             </CardContent>
         </Card>
-    )
+    );
 }
