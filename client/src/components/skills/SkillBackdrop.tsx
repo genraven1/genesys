4367d2a6c21fe7fb75d326b4de304a68b115f224
genraven1 <genraven1@outlook.com>
2024-10-02 11:01:19 -0500

    Remove Edit and View Skill Pages

--- client/src/components/skills/SkillBackdrop.tsx
+++ client/src/components/skills/SkillBackdrop.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import Backdrop from '@mui/material/Backdrop';
 import Skill from "../../models/actor/Skill";
-import SkillView from "./SkillView";
+import {Card, CardContent, CardHeader, Grid} from "@mui/material";
+import {ViewFieldCard} from "../common/ViewFieldCard";
 
 interface Props {
     skill: Skill
@@ -14,7 +15,15 @@ export default function SkillBackdrop(props: Props) {
 
     return (
         <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={open} onClick={onClose}>
-            <SkillView skill={skill}/>
+            <Card>
+                <CardHeader style={{textAlign: 'center'}} title={skill.name}/>
+                <CardContent>
+                    <Grid container justifyContent={'center'}>
+                        <ViewFieldCard name={'Skill Type'} value={skill.type} />
+                        <ViewFieldCard name={'Characteristic'} value={skill.characteristic} />
+                    </Grid>
+                </CardContent>
+            </Card>
         </Backdrop>
     );
 }
