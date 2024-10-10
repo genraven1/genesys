32d1f85bbe93a2c1971ab9edd8b0df9bd026c27a
genraven1 <genraven1@outlook.com>
2024-10-10 15:12:48 -0500

    Refactor Armor Workflow

--- client/src/components/equipment/armor/ArmorWorkflow.tsx
+++ client/src/components/equipment/armor/ArmorWorkflow.tsx
@@ -1,50 +1,13 @@
-import {Fragment, useEffect, useState} from "react";
-import {useLocation, useParams} from "react-router-dom";
-import {Armor} from "../../../models/equipment/Armor";
-import EquipmentService from "../../../services/EquipmentService";
-import ArmorView from "./ArmorView";
-import ArmorEdit from "./ArmorEdit";
+import {Fragment} from "react";
+import {useLocation} from "react-router-dom";
 import ViewAllArmor from "./ViewAllArmor";
-
-
-function useFetchArmor(name: string): Armor {
-    const [armor, setArmor] = useState<Armor>()
-    useEffect(() => {
-        if (!name) {
-            return
-        }
-        (async (): Promise<void> => {
-            try {
-                const armorData = await EquipmentService.getArmor(name)
-                if (armorData) {
-                    setArmor(armorData)
-                }
-            } catch (err) {
-                console.log(err)
-            }
-        })()
-    }, [name, setArmor])
-    return armor as Armor
-}
+import {EquipmentPath} from "../../../services/Path";
+import ArmorPage from "./ArmorPage";
 
 export default function ArmorWorkflow() {
-    const {name} = useParams<{ name: string }>()
-    const armor = useFetchArmor(name as string)
-
-    const useWorkflowRender = () => {
-        const pathname = useLocation().pathname
-        if (pathname.endsWith('/view')) {
-            return armor && <ArmorView armor={armor}/>
-        } else if (pathname.endsWith('/edit')) {
-            return armor && <ArmorEdit ar={armor}/>
-        } else {
-            return <ViewAllArmor/>
-        }
-    }
-
     return (
         <Fragment>
-            {useWorkflowRender()}
+            {useLocation().pathname.endsWith(EquipmentPath.Armor) ? <ViewAllArmor/> : <ArmorPage/>}
         </Fragment>
     )
 }
