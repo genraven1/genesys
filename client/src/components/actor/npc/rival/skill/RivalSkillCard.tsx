d6c0e9bc46afe4cd51205a794daf9ba758150e23
genraven1 <genraven1@outlook.com>
2024-10-25 19:12:47 -0500

    Update Rival Skill Card

--- client/src/components/actor/npc/rival/skill/RivalSkillCard.tsx
+++ client/src/components/actor/npc/rival/skill/RivalSkillCard.tsx
@@ -1,10 +1,44 @@
-import {Card, CardContent} from "@mui/material";
+import {Card, CardContent, Grid} from "@mui/material";
 import {useLocation} from "react-router-dom";
-import {Fragment} from "react";
 import CenteredCardHeader from "../../../../common/card/CenteredCardHeader";
-import ViewRivalSkillTable from "./ViewRivalSkillTable";
-import RivalSkillTable from "./RivalSkillTable";
 import Rival from "../../../../../models/actor/npc/Rival";
+import TableContainer from "@mui/material/TableContainer";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import {SkillType} from "../../../../../models/actor/Skill";
+import * as React from "react";
+import {renderDoubleRowTableHeader, renderSkillName} from "../../../../common/table/TableRenders";
+import TableRow from "@mui/material/TableRow";
+import {GenesysDicePoolCenterTableCell, TypographyCenterTableCell} from "../../../../common/table/TypographyTableCell";
+import {ActorSkill} from "../../../../../models/actor/Actor";
+import NumberTextFieldTableCell from "../../../../common/table/NumberTextFieldTableCell";
+
+
+interface GroupProps {
+    rival: Rival
+    type: SkillType
+}
+
+function SkillTypeGroup(props: GroupProps) {
+    const {rival, type} = props;
+    let headers = ['Skill', 'Ranks', 'Dice Pool'];
+
+    return (
+        <Table>
+            {renderDoubleRowTableHeader(headers, type, 3)}
+            <TableBody>
+                {(rival.skills || []).filter((skill) => skill.type === type).map((skill: ActorSkill) => (
+                    <TableRow key={skill.name}>
+                        {renderSkillName(skill)}
+                        <TypographyCenterTableCell value={String(skill.ranks)}/>
+                        <GenesysDicePoolCenterTableCell actor={rival} skill={skill}/>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    );
+}
 
 interface Props {
     rival: Rival
@@ -14,22 +48,33 @@ export default function RivalSkillCard(props: Props): JSX.Element {
     const {rival} = props
     const pathname = useLocation().pathname
 
-    const renderSkillTable = (): JSX.Element => {
-        if (pathname.endsWith('/view')) {
-            return <ViewRivalSkillTable rival={rival}/>
-        } else if (pathname.endsWith('/edit')) {
-            return <RivalSkillTable rival={rival}/>
-        } else {
-            return <Fragment/>
-        }
-    }
-
-
     return (
         <Card sx={{"width": 1}}>
             <CenteredCardHeader title={'Skills'}/>
             <CardContent>
-                {renderSkillTable()}
+                <Grid container>
+                    <Grid item xs={6}>
+                        <TableContainer component={Paper}>
+                            <Table>
+                                <TableBody>
+                                    <SkillTypeGroup rival={rival} type={SkillType.General}/>
+                                    <SkillTypeGroup rival={rival} type={SkillType.Magic}/>
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TableContainer component={Paper}>
+                            <Table>
+                                <TableBody>
+                                    <SkillTypeGroup rival={rival} type={SkillType.Combat}/>
+                                    <SkillTypeGroup rival={rival} type={SkillType.Social}/>
+                                    <SkillTypeGroup rival={rival} type={SkillType.Knowledge}/>
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Grid>
+                </Grid>
             </CardContent>
         </Card>
     )
