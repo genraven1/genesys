401d6eb1b148cde70e7986ccb7d4ed9d70d3e56c
genraven1 <genraven1@outlook.com>
2024-11-09 10:42:36 -0600

    Update Actor Dashboard

--- client/src/components/home/ActorDashboard.tsx
+++ client/src/components/home/ActorDashboard.tsx
@@ -8,9 +8,6 @@ import {ActorType} from "../../models/actor/Actor";
 import {ActorPath} from "../../services/RootPath";
 
 export default function ActorDashboard() {
-    const [openMinionCreationDialog, setOpenMinionCreationDialog] = useState(false)
-    const [openRivalCreationDialog, setOpenRivalCreationDialog] = useState(false)
-    const [openNemesisCreationDialog, setOpenNemesisCreationDialog] = useState(false)
     const [openPlayerCreationDialog, setOpenPlayerCreationDialog] = useState(false)
 
     return (
@@ -18,29 +15,11 @@ export default function ActorDashboard() {
             <CenteredCardHeader title={'Actor MainDashboard'}/>
             <CardContent>
                 <Grid container justifyContent={'center'}>
-                    <ExpansionList header={'View Minions'} viewTitle={'View Minions'} to={ActorPath.Minion}
-                                   dialogTitle={'Create Minions'}
-                                   onClick={(): void => setOpenMinionCreationDialog(true)}/>
-                    <ExpansionList header={'View Rivals'} viewTitle={'View Rivals'} to={ActorPath.Rival}
-                                   dialogTitle={'Create Rival'}
-                                   onClick={(): void => setOpenRivalCreationDialog(true)}/>
-                    <ExpansionList header={'View Nemeses'} viewTitle={'View Nemeses'} to={ActorPath.Nemesis}
-                                   dialogTitle={'Create Nemesis'}
-                                   onClick={(): void => setOpenNemesisCreationDialog(true)}/>
                     <ExpansionList header={'View Players'} viewTitle={'View Players'} to={ActorPath.Player}
                                    dialogTitle={'Create Player'}
                                    onClick={(): void => setOpenPlayerCreationDialog(true)}/>
                 </Grid>
             </CardContent>
-            {openMinionCreationDialog && <CreateActorDialog open={openMinionCreationDialog}
-                                                            onClose={(): void => setOpenMinionCreationDialog(false)}
-                                                            actorType={ActorType.Minion}/>}
-            {openRivalCreationDialog && <CreateActorDialog open={openRivalCreationDialog}
-                                                           onClose={(): void => setOpenRivalCreationDialog(false)}
-                                                           actorType={ActorType.Rival}/>}
-            {openNemesisCreationDialog && <CreateActorDialog open={openNemesisCreationDialog}
-                                                             onClose={(): void => setOpenNemesisCreationDialog(false)}
-                                                             actorType={ActorType.Nemesis}/>}
             {openPlayerCreationDialog && <CreateActorDialog open={openPlayerCreationDialog}
                                                             onClose={(): void => setOpenPlayerCreationDialog(false)}
                                                             actorType={ActorType.Player}/>}
