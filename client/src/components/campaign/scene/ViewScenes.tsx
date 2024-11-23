aea13045b1cd0287897993bdf4229793a8ca6823
genraven1 <genraven1@outlook.com>
2024-11-23 15:09:20 -0600

    Clean up unneeded files

--- client/src/components/campaign/scene/ViewScenes.tsx
+++ client/src/components/campaign/scene/ViewScenes.tsx
@@ -1,9 +1,17 @@
-import {Card} from "@mui/material";
+import {Card, CardContent} from "@mui/material";
+import CenteredCardHeaderWithDialog from "../../common/card/header/CenteredCardHeaderWithDialog";
+import {useState} from "react";
 
 export default function ViewScenes() {
+    const [openSceneDialog, setOpenSceneDialog] = useState(false);
+
     return (
         <Card>
+            <CenteredCardHeaderWithDialog title={'View All Scenes'} onClick={() => setOpenSceneDialog(true)}
+                                          buttonText={'Create Scene'}/>
+            <CardContent>
 
+            </CardContent>
         </Card>
     )
 }
