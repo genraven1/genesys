6d0abe40db9c4455ec0a28a2f3a651a1063de791
genraven1 <genraven1@outlook.com>
2024-12-21 23:37:04 -0600

    Create Full Talent Pyramid

--- client/src/components/campaign/party/player/experience/dialog/SpendTalentDialog.tsx
+++ client/src/components/campaign/party/player/experience/dialog/SpendTalentDialog.tsx
@@ -1,8 +1,11 @@
 import Player from "../../../../../../models/actor/player/Player";
-import {useEffect, useState} from "react";
-import {Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid, IconButton} from "@mui/material";
 import * as React from "react";
+import {useEffect, useState} from "react";
+import {Button, Card, CardContent, Dialog, DialogActions, DialogContent, DialogTitle, Grid} from "@mui/material";
 import PlayerService from "../../../../../../services/actor/PlayerService";
+import {ActorTalent, Tier} from "../../../../../../models/Talent";
+import CenteredCardHeader from "../../../../../common/card/header/CenteredCardHeader";
+import GenesysDescriptionTypography from "../../../../../common/typography/GenesysDescriptionTypography";
 
 interface Props {
     open: boolean
@@ -13,6 +16,12 @@ interface Props {
 export default function SpendTalentDialog(props: Props) {
     const {open, onClose, currentPlayer} = props;
     const [player, setPlayer] = useState(currentPlayer);
+    const firstTalents = player.talents.filter(talent => talent.tier === Tier.First);
+    const secondTalents = player.talents.filter(talent => talent.tier === Tier.Second);
+    const thirdTalents = player.talents.filter(talent => talent.tier === Tier.Third);
+    const fourthTalents = player.talents.filter(talent => talent.tier === Tier.Fourth);
+    const fifthTalents = player.talents.filter(talent => talent.tier === Tier.Fifth);
+
 
     useEffect(() => {
         setPlayer(currentPlayer);
@@ -23,12 +32,42 @@ export default function SpendTalentDialog(props: Props) {
         onClose();
     };
 
+    const renderTalentPaper = (talents: ActorTalent[]) => {
+        return (
+            <Grid container direction="column" spacing={2}>
+                {talents.map((talent) => (
+                    <Grid item key={talent.id}>
+                        <Card>
+                            <CenteredCardHeader title={talent.name}/>
+                            <CardContent>
+                                <GenesysDescriptionTypography text={talent.summary}/>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>);
+    };
+
     return (
         <Dialog open={open} onClose={onClose} fullScreen>
-            <DialogTitle>Spend Experience on Talent</DialogTitle>
+            <DialogTitle style={{textAlign: "center"}}>Spend Experience on Talent</DialogTitle>
             <DialogContent>
                 <Grid container spacing={2} columns={5}>
-
+                    <Grid item xs={1}>
+                        {renderTalentPaper(firstTalents)}
+                    </Grid>
+                    <Grid item xs={1}>
+                        {renderTalentPaper(secondTalents)}
+                    </Grid>
+                    <Grid item xs={1}>
+                        {renderTalentPaper(thirdTalents)}
+                    </Grid>
+                    <Grid item xs={1}>
+                        {renderTalentPaper(fourthTalents)}
+                    </Grid>
+                    <Grid item xs={1}>
+                        {renderTalentPaper(fifthTalents)}
+                    </Grid>
                 </Grid>
             </DialogContent>
             <DialogActions>
