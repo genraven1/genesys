0859b15fa8382a514cc0d5a994cbac1035195d6d
genraven1 <genraven1@outlook.com>
2025-01-09 16:39:14 -0600

    Talent Activation Button

--- client/src/components/campaign/party/player/talent/PlayerTalentCard.tsx
+++ client/src/components/campaign/party/player/talent/PlayerTalentCard.tsx
@@ -12,14 +12,15 @@ import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import {renderSingleRowTableHeader} from "../../../../common/table/TableRenders";
 import * as React from "react";
+import TalentActivationTableCell from "../../../../common/table/TalentActivationTableCell";
 
 interface Props {
     talents: ActorTalent[]
 }
 
-export default function PlayerTalentCard(props: Props): JSX.Element {
+export default function PlayerTalentCard(props: Props) {
     const {talents} = props;
-    const headers = ['Name', 'Activation', 'Summary'];
+    const headers = ['Name', 'Activation', 'Summary', 'Activate'];
 
     const renderTableBody = () => {
         return (
@@ -29,6 +30,7 @@ export default function PlayerTalentCard(props: Props): JSX.Element {
                         <TypographyCenterTableCell value={talent.name}/>
                         <TypographyCenterTableCell value={talent.activation}/>
                         <GenesysDescriptionTypographyCenterTableCell value={talent.summary}/>
+                        <TalentActivationTableCell talent={talent}/>
                     </TableRow>
                 ))}
             </TableBody>
