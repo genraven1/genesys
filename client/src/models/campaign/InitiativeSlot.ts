ecd9f9a2ac4799d505e241514ac57886b873739a
genraven1 <genraven1@outlook.com>
2025-01-15 09:59:37 -0600

    Add Character Class

--- client/src/models/campaign/InitiativeSlot.ts
+++ client/src/models/campaign/InitiativeSlot.ts
@@ -1,8 +1,10 @@
 import {GenesysSymbols} from "../roll/GenesysSymbols";
+import Character from "../actor/Character";
 
 export default interface InitiativeSlot {
     type: Type
     results: Record<GenesysSymbols, number>
+    character: Character
 }
 
 export enum Type {
